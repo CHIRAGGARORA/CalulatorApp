aec201787a13a8d2cc4988dae5fff051697bce3a
Chirag Arora <arora2002chirag@gmail.com>
2023-01-05 02:48:35 +0530

    Initial Commit

---
+++ CalulatorApp/CalulatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalulatorAppApp.swift
+//  CalulatorApp
+//
+//  Created by chirag arora on 05/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CalulatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
